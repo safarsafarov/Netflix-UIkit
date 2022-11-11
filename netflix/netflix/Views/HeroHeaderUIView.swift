179867006459282a70429725f075e88e3c230cd3
Safar Safarov <Safarov.safar@hotmail.com>
2022-11-11 18:28:15 +0500

    ✨

---
+++ netflix/netflix/Views/HeroHeaderUIView.swift
@@ -0,0 +1,34 @@
+//
+//  HeroHeaderUIView.swift
+//  netflix
+//
+//  Created by Safar Safarov on 17/11/22.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+}
