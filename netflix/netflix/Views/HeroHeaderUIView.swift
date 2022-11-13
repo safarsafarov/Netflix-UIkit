dd78e58f4e860424b71967257fc4295ec3b7e1ea
Safar Safarov <Safarov.safar@hotmail.com>
2022-11-13 19:03:13 +0500

    ✨

--- netflix/netflix/Views/HeroHeaderUIView.swift
+++ netflix/netflix/Views/HeroHeaderUIView.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 class HeroHeaderUIView: UIView {
+    
+    private let playButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.systemBackground.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     private let heroImageView: UIImageView = {
         let imageView = UIImageView()
@@ -17,9 +26,28 @@ class HeroHeaderUIView: UIView {
         return imageView
     }()
     
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(heroImageView)
+        addGradient()
+        addSubview(playButton)
+        applyContraints()
+    }
+    
+    private func applyContraints() {
+        let playButtonConstraints = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20)]
     }
     
     override func layoutSubviews() {
